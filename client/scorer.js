741acaf4f83b95a048aee237f8fbebcb744621b1
shanebishop <shane.bishop@outlook.com>
2019-12-18 16:00:55 -0500

    Added initial tests for Scorer class

--- client/scorer.js
+++ client/scorer.js
@@ -1,3 +1,5 @@
+var _ = require('underscore');
+
 function Scorer () {
     this._scored = new Set();
 }
@@ -64,17 +66,147 @@ Scorer.prototype.score = function (dieRoll, scoreCode) {
 
     if (isUpperScoreCode(scoreCode)) {
         score = scoreForUpperSection(dieRoll, scoreCode);
-    } else if (scoreCode === LGSTRAIGHT_CODE) {
+    } else if (scoreCode === Scorer.LGSTRAIGHT_CODE) {
         score = scoreAsLargeStraight(dieRoll);
+    } else if (scoreCode === Scorer.SMSTRAIGHT_CODE) {
+        score = scoreAsSmallStraight(dieRoll);
+    } else if (scoreCode === Scorer.FULLHOUSE_CODE) {
+        score = scoreAsFullHouse(dieRoll);
+    } else if (scoreCode === Scorer.THREEKIND_CODE) {
+        score = scoreAsThreeKind(dieRoll);
+    } else if (scoreCode === Scorer.FOURKIND_CODE) {
+        score = scoreAsFourKind(dieRoll);
+    } else if (scoreCode === Scorer.CHANCE_CODE) {
+        score = sumOfRoll(dieRoll);
+    } else if (scoreCode === Scorer.FRSTYAHTZEE_CODE) {
+        score = scoreAsFirstYahtzee(dieRoll);
+    } else if (scoreCode === Scorer.SCNDYAHTZEE_CODE) {
+        score = scoreAsSecondYahtzee(dieRoll);
+    } else {
+        throw new Error('Invalid score code: \'' + scoreCode + '\'');
     }
-    // TODO Rest of cases
 
     this._scored.add(scoreCode);
     return score;
 };
 
+function scoreAsLargeStraight (dieRoll) {
+    return hasNConsecutiveValues(dieRoll, Scorer._LGSTRAIGHT_CONSECUTIVE) ? Scorer.LGSTRAIGHT_SCORE : 0;
+}
+
+function scoreAsSmallStraight (dieRoll) {
+    return hasNConsecutiveValues(dieRoll, Scorer._SMSTRAIGHT_CONSECUTIVE) ? Scorer.SMSTRAIGHT_SCORE : 0;
+}
+
+function scoreAsFullHouse (dieRoll) {
+    return isFullHouse(dieRoll) ? Scorer.FULLHOUSE_SCORE : 0;
+}
+
+function sumOfRoll (dieRoll) {
+    var sum = 0;
+    for (var i = 0; i < dieRoll.length; ++i) {
+        sum += dieRoll[i];
+    }
+    return sum;
+}
+
+// Returns true if the roll contains at least n values that are the same; otherwise false
+function atLeastNTheSame (dieRoll, n) {
+    return max(_.values(getCountPerValue(dieRoll))) >= n;
+}
+
+function max (array) {
+    var max = array[0];
+    for (var i = 1; i < array.length; ++i) {
+        if (array[i] > max) {
+            max = array[i];
+        }
+    }
+    return max;
+}
+
+function scoreAsThreeKind (dieRoll) {
+    return hasThreeOrMoreSame(dieRoll) ? sumOfRoll(dieRoll) : 0;
+}
+
+function scoreAsFourKind (dieRoll) {
+    return hasFourOrMoreSame(dieRoll) ? sumOfRoll(dieRoll) : 0;
+}
+
+function scoreAsFirstYahtzee (dieRoll) {
+    return isYahtzee(dieRoll) ? Scorer.FRSTYAHTZEE_SCORE : 0;
+}
+
+function scoreAsSecondYahtzee (dieRoll) {
+    return isYahtzee(dieRoll) ? Scorer.SCNDYAHTZEE_SCORE : 0;
+}
+
 function isUpperScoreCode (scoreCode) {
     return Scorer.upperScoreCodes.has(scoreCode);
+}
+
+// Returns true if category is a valid category (is a number in [1,13]), and false otherwise
+Scorer.isValidCategory = function (categoryStr) {
+    var category = parseInt(categoryStr, 10);
+
+    // parseInt returns a Number rather than an int. parseInt also floors its
+    // input in order to return an integral, so we must confirm the original
+    // string is integral.
+    return isInt(categoryStr) && category >= 1 && category <= Scorer.NUM_CATEGORIES;
+}
+
+// Returns true if value is an integral value, even if value is a string
+function isInt (value) {
+    return !isNaN(value) &&
+         parseInt(Number(value)) == value && // Must use == instead of === here
+         !isNaN(parseInt(value, 10));
+}
+
+// Returns a map representing corresponding the die values rolled to the number of
+// dice with the same value. The keys are the die values rolled, and the values are
+// the number of dice rolled with that value. For convenience, there is a key for every
+// possible die value.
+//
+// For example, a roll of {5, 4, 5, 3, 1} would return a map of
+// {1: 1, 2: 0, 3: 1, 4: 1, 5: 2, 6: 0}
+function getCountPerValue (dieRoll) {
+    var ret = {};
+    var minDieRoll = 1, maxDieRoll = 6, i;
+
+    // Set every key's corresponding value to 0
+    for (i = minDieRoll; i <= maxDieRoll; ++i) {
+        ret[i] = 0;
+    }
+
+    for (i = 0; i < dieRoll.length; ++i) {
+        var value = dieRoll[i];
+        ret[value] = ret[value] + 1;
+    }
+
+    return ret;
+}
+
+// Returns true if the die roll is a possible yahtzee
+function isYahtzee (dieRoll) {
+    return _.values(getCountPerValue(dieRoll)).includes(Scorer.NUM_SAME_FOR_YAHTZEE);
+}
+
+// Returns true if the die roll is a four of a kind
+function hasFourOrMoreSame (dieRoll) {
+    return atLeastNTheSame(dieRoll, 4);
+}
+
+// Returns true if the die roll is a three of a kind
+function hasThreeOrMoreSame (dieRoll) {
+    return atLeastNTheSame(dieRoll, 3);
+}
+
+// Returns true if the die roll is a full house
+function isFullHouse (dieRoll) {
+    var counts = _.values(getCountPerValue(dieRoll));
+
+    // A full house requires two of a kind and three of a kind
+    return counts.includes(2) && counts.includes(3);
 }
 
 function scoreForUpperSection (dieRoll, scoreCode) {
@@ -85,7 +217,6 @@ function scoreForUpperSection (dieRoll, scoreCode) {
     codeToDieRoll[Scorer.FOURS_CODE] = 4;
     codeToDieRoll[Scorer.FIVES_CODE] = 5;
     codeToDieRoll[Scorer.SIXES_CODE] = 6;
-    console.log(codeToDieRoll);
 
     var score = 0;
     for (var i = 0; i < dieRoll.length; ++i) {
@@ -95,6 +226,45 @@ function scoreForUpperSection (dieRoll, scoreCode) {
     }
 
     return score;
+}
+
+// Returns true if dieRoll contains a permutation with n or more consecutive numbers.
+//
+// The logic is a sorted list of integers would cause larger integers to come after
+// smaller integers. If integers at position i and i+1 in the list have a difference
+// of 1, then those two items are consecutive. Thus, if a counter reaches n,
+// there are n consecutive values in a permutation of the list.
+function hasNConsecutiveValues (dieRoll, n) {
+    var dieRollCopy = arrayCopy(dieRoll);
+    dieRollCopy.sort(function (a, b) { return a - b; });
+    var counter = 1; // counter starts at (and is reset to) 1, since any value is a consecutive list of size 1
+    var lastValue = dieRollCopy[0];
+
+    // i must start at 1 (if i started at 0, then 0-1 = -1)
+    for (var i = 1; i < dieRollCopy.length; ++i) {
+        if (dieRollCopy[i] === lastValue) {
+            // Do nothing
+        } else if (dieRollCopy[i] - dieRollCopy[i-1] === 1) {
+            ++counter;
+            if (counter === n) {
+                return true;
+            }
+        } else {
+            counter = 1;
+        }
+        lastValue = dieRollCopy[i];
+    }
+
+    // Counter never reached n, so there is not n consecutive values
+    return false;
+}
+
+function arrayCopy (array) {
+    var ret = []
+    for (var i = 0; i < array.length; ++i) {
+        ret[i] = array[i];
+    }
+    return ret;
 }
 
 module.exports = Scorer;
--- test/client/scorer.js
+++ test/client/scorer.js
@@ -4,8 +4,8 @@ var Scorer = require('../../client/scorer')
 describe('Scorer', function () {
 
     it('Test scoring in ones with ones', function () {
-        s = new Scorer();
-        dieRolls = [1, 1, 3, 1, 5];
+        var s = new Scorer();
+        var dieRolls = [1, 1, 3, 1, 5];
         var actual = s.score(dieRolls, Scorer.ONES_CODE);
 
         assert.equal(3, actual);
@@ -13,12 +13,207 @@ describe('Scorer', function () {
     });
 
     it('Test scoring in twos with no twos', function () {
-        s = new Scorer();
-        dieRolls = [5, 6, 4, 3, 4];
+        var s = new Scorer();
+        var dieRolls = [5, 6, 4, 3, 4];
         var actual = s.score(dieRolls, Scorer.TWOS_CODE);
 
         assert.equal(0, actual);
         assert.isTrue(s.scored(Scorer.TWOS_CODE));
+    });
+
+    it('Test isValidCategory', function () {
+        assert.isFalse(Scorer.isValidCategory("-1"));
+        assert.isFalse(Scorer.isValidCategory("0"));
+
+        assert.equal(Scorer.NUM_CATEGORIES, 13);
+
+        for (var i = 1; i <= Scorer.NUM_CATEGORIES; ++i) {
+            assert.isTrue(Scorer.isValidCategory(i.toString()),
+                    'Value in [1,13] was not seen as a valid category');
+        }
+
+        assert.isFalse(Scorer.isValidCategory("2.5"), '2.5 was seen as a valid category');
+        assert.isFalse(Scorer.isValidCategory("a"));
+        assert.isFalse(Scorer.isValidCategory("str"));
+        assert.isFalse(Scorer.isValidCategory(""));
+        assert.isFalse(Scorer.isValidCategory(null));
+    });
+
+    it('Can score in ones category', function () {
+        var s = new Scorer();
+        var dieRolls = [1, 1, 1, 3, 5];
+        var scored = s.score(dieRolls, Scorer.ONES_CODE);
+        assert.equal(3, scored);
+    });
+
+    it('Can score in twos category', function () {
+        var s = new Scorer();
+        var dieRolls = [2, 2, 2, 3, 5];
+        var scored = s.score(dieRolls, Scorer.TWOS_CODE);
+        assert.equal(6, scored);
+    });
+
+    it('Can score in threes category', function () {
+        var s = new Scorer();
+        var dieRolls = [2, 2, 2, 3, 5];
+        var scored = s.score(dieRolls, Scorer.THREES_CODE);
+        assert.equal(3, scored);
+    });
+
+    it('Can score in fours category', function () {
+        var s = new Scorer();
+        var dieRolls = [4, 4, 4, 3, 5];
+        var scored = s.score(dieRolls, Scorer.FOURS_CODE);
+        assert.equal(12, scored);
+    });
+
+    it('Can score in fives category', function () {
+        var s = new Scorer();
+        var dieRolls = [2, 2, 2, 3, 5];
+        var scored = s.score(dieRolls, Scorer.FIVES_CODE);
+        assert.equal(5, scored);
+    });
+
+    it('Can score in sixes category', function () {
+        var s = new Scorer();
+        var dieRolls = [6, 6, 6, 3, 5];
+        var scored = s.score(dieRolls, Scorer.SIXES_CODE);
+        assert.equal(18, scored);
+    });
+
+    it('Can score in large straight category', function () {
+        var s = new Scorer();
+        var dieRolls = [1, 2, 3, 4, 5];
+        var scored = s.score(dieRolls, Scorer.LGSTRAIGHT_CODE);
+        assert.equal(Scorer.LGSTRAIGHT_SCORE, scored);
+    });
+
+    it('Can score in large straight category - reversed', function () {
+        var s = new Scorer();
+        var dieRolls = [6, 5, 4, 3, 2];
+        var scored = s.score(dieRolls, Scorer.LGSTRAIGHT_CODE);
+        assert.equal(Scorer.LGSTRAIGHT_SCORE, scored);
+    });
+
+    it('Can score in large straight category - scrambled', function () {
+        var s = new Scorer();
+        var dieRolls = [5, 3, 2, 4, 1];
+        var scored = s.score(dieRolls, Scorer.LGSTRAIGHT_CODE);
+        assert.equal(Scorer.LGSTRAIGHT_SCORE, scored);
+    });
+
+    it('Can score in small straight category', function () {
+        var s = new Scorer();
+        var dieRolls = [1, 2, 3, 4, 6];
+        var scored = s.score(dieRolls, Scorer.SMSTRAIGHT_CODE);
+        assert.equal(Scorer.SMSTRAIGHT_SCORE, scored);
+    });
+
+    it('Can score in small straight category - duplicates in roll', function () {
+        var s = new Scorer();
+        var dieRolls = [3, 6, 5, 4, 5];
+        var scored = s.score(dieRolls, Scorer.SMSTRAIGHT_CODE);
+        assert.equal(Scorer.SMSTRAIGHT_SCORE, scored);
+    });
+
+    it('Can score in small straight category - reversed', function () {
+        var s = new Scorer();
+        var dieRolls = [1, 6, 5, 4, 3];
+        var scored = s.score(dieRolls, Scorer.SMSTRAIGHT_CODE);
+        assert.equal(Scorer.SMSTRAIGHT_SCORE, scored);
+    });
+
+    it('Can score in small straight category - scrambled', function () {
+        var s = new Scorer();
+        var dieRolls = [1, 4, 5, 6, 3];
+        var scored = s.score(dieRolls, Scorer.SMSTRAIGHT_CODE);
+        assert.equal(Scorer.SMSTRAIGHT_SCORE, scored);
+    });
+
+    it('Can score in small straight category when a large straight is also possible', function () {
+        var s = new Scorer();
+        var dieRolls = [6, 5, 4, 3, 2];
+        var scored = s.score(dieRolls, Scorer.SMSTRAIGHT_CODE);
+        assert.equal(Scorer.SMSTRAIGHT_SCORE, scored);
+    });
+
+    it('Can score in full house category', function () {
+        var s = new Scorer();
+        var dieRolls = [2, 2, 2, 3, 3];
+        var scored = s.score(dieRolls, Scorer.FULLHOUSE_CODE);
+        assert.equal(Scorer.FULLHOUSE_SCORE, scored);
+    });
+
+    it('Can score in three of a kind category', function () {
+        var s = new Scorer();
+        var dieRolls = [2, 2, 2, 3, 5];
+
+        var rollSum = 0;
+        for (var i = 0; i < dieRolls.length; ++i) {
+            rollSum += dieRolls[i];
+        }
+
+        var scored = s.score(dieRolls, Scorer.THREEKIND_CODE);
+        assert.equal(rollSum, scored);
+    });
+
+    it('Can score in three of a kind category when die roll is four of a kind', function () {
+        var s = new Scorer();
+        var dieRolls = [1, 1, 1, 1, 3];
+
+        var rollSum = 0;
+        for (var i = 0; i < dieRolls.length; ++i) {
+            rollSum += dieRolls[i];
+        }
+
+        var scored = s.score(dieRolls, Scorer.THREEKIND_CODE);
+        assert.equal(rollSum, scored);
+    });
+
+    it('Can score in four of a kind category', function () {
+        var s = new Scorer();
+        var dieRolls = [6, 4, 4, 4, 4];
+
+        var rollSum = 0;
+        for (var i = 0; i < dieRolls.length; ++i) {
+            rollSum += dieRolls[i];
+        }
+
+        var scored = s.score(dieRolls, Scorer.FOURKIND_CODE);
+        assert.equal(rollSum, scored);
+    });
+
+    it('Can score in four of a kind category when die roll is 5 of a kind', function () {
+        var s = new Scorer();
+        var dieRolls = [5, 5, 5, 5, 5];
+
+        var rollSum = 0;
+        for (var i = 0; i < dieRolls.length; ++i) {
+            rollSum += dieRolls[i];
+        }
+
+        var scored = s.score(dieRolls, Scorer.FOURKIND_CODE);
+        assert.equal(rollSum, scored);
+    });
+
+    it('Can score in chance category', function () {
+        var s = new Scorer();
+        var dieRolls = [2, 5, 1, 3, 5];
+
+        var rollSum = 0;
+        for (var i = 0; i < dieRolls.length; ++i) {
+            rollSum += dieRolls[i];
+        }
+
+        var scored = s.score(dieRolls, Scorer.CHANCE_CODE);
+        assert.equal(rollSum, scored);
+    });
+
+    it('Can score in score first yahtzee in yahtzee cateogory', function () {
+        var s = new Scorer();
+        var dieRolls = [3, 3, 3, 3, 3];
+        var scored = s.score(dieRolls, Scorer.FRSTYAHTZEE_CODE);
+        assert.equal(Scorer.FRSTYAHTZEE_SCORE, scored);
     });
 
 });
