85cad1cb7508af61e75ca5de89158be25ac49e25
Shane Bishop <shane.bishop@outlook.com>
2019-12-22 17:34:08 -0500

    Some unsuccessful work on updating the die roll display

--- src/App.js
+++ src/App.js
@@ -11,23 +11,35 @@ function main () {
     var scorer = new Scorer();
     scorer.setScored(Scorer.ONES_CODE);
     console.log('Scored in ones? ', scorer.scored(Scorer.ONES_CODE));
-}
 
-main();
+    var view = new View();
+    view.setRolled([6,6,6,6,6]);
+    console.log('Calling view.render()');
+    view.render();
+
+    //updateView(view);
+    setTimeout(updateView.bind(null, view), 4000);
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Hello, I have text
-        </p>
-      </header>
+      <header className="App-header">{/* Currently unused */}</header>
       <div>
         <View />
       </div>
     </div>
   );
 }
+
+function updateView (view) {
+    view.setRolled([6, 6, 6, 6, 6]);
+    view.render();
+    //view.setState({ rolled : [6, 6, 6, 6, 6, 6] });
+    view.setRolled([6, 6, 6, 6, 6]);
+    //setTimeout(updateView.bind(null, view), 4000);
+}
+
+main();
 
 export default App;
--- src/client/view.js
+++ src/client/view.js
@@ -9,7 +9,7 @@ class Die extends React.Component {
     render () {
         return (
             <img
-                src="img/die1.png"
+                src={"img/die" + this.props.value +".png"}
                 alt="my image"
                 height="70"
                 width="70"
@@ -20,19 +20,36 @@ class Die extends React.Component {
 }
 
 class Board extends React.Component {
+    constructor (props) {
+        super(props);
+
+        props = props || {};
+
+        this._rolled = props.rolled;
+        this.state = {};
+        this.state.rolled = props.rolled;
+        //this.setState(props);
+    }
+
     renderDie (i) {
-        return <Die value={i} />;
+        //return <Die value={i} />;
+        return <Die value={this.props.rolled[i]} />
+    }
+
+    setRolled (rolled) {
+        this._rolled = rolled;
+        this.state.rolled = rolled;
     }
 
     render () {
         return (
             <div>
                 <div className="die-rolls">
+                    {this.renderDie(0)}
                     {this.renderDie(1)}
                     {this.renderDie(2)}
                     {this.renderDie(3)}
                     {this.renderDie(4)}
-                    {this.renderDie(5)}
                 </div>
             </div>
         );
@@ -40,13 +57,35 @@ class Board extends React.Component {
 }
 
 class View extends React.Component {
+    constructor () {
+        super();
+        this._rolled = [4, 3, 5, 2, 5];
+        this.state = {rolled : this._rolled};
+    }
+
+    setRolled (rolled) {
+        this._rolled = rolled;
+        //this._board.setRolled(rolled);
+        this.state = {rolled : this._rolled};
+        //this.setState({rolled : rolled});
+        console.log('this._rolled is now', this._rolled);
+    }
+
     render () {
+        const rolled = this.state.rolled;
+        console.log('Attempting to render view with rolled', this.state.rolled);
         return (
             <div className="game-board">
-                <Board />
+                <Board rolled={rolled} />
+                <button onClick={newRoll.bind(this)}>{'asdfasdf'}</button>
             </div>
         );
     }
+}
+
+function newRoll () {
+    this.setRolled([6,6,6,6,6]);
+    this.render();
 }
 
 ReactDOM.render(<View />, document.getElementById('root'));
