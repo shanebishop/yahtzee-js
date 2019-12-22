2a1dfe795bd2d0973de2d95ba506c981e995c3d1
Shane Bishop <shane.bishop@outlook.com>
2019-12-22 13:05:09 -0500

    React page displays

---
+++ src/App.js
@@ -0,0 +1,29 @@
+'use strict';
+
+import React from 'react';
+//import './App.css'; // Uncomment to import CSS for App
+
+var Scorer = require('./client/scorer');
+
+function main () {
+    console.log('Hello world');
+    var scorer = new Scorer();
+    scorer.setScored(Scorer.ONES_CODE);
+    console.log('Scored in ones? ', scorer.scored(Scorer.ONES_CODE));
+}
+
+main();
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hello, I have text
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import './index.css'; // Uncomment to use CSS from index.css
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
